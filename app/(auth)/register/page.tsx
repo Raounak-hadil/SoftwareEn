301e7cb50fb9db564b15550bd1a3c29b2e428b7a
Raounak Hadil Kaoua <raounak.hadil.kaoua@ensia.edu.dz>
2026-01-02 21:19:45 +0100

    Update page.tsx

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,20 +1,26 @@
 "use client";
-
 import SignIn from "./SignIn1";
 import SignIn2 from "./SignIn2";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function RegisterPage() {
+function RegisterContent() {
   const searchParams = useSearchParams();
   const type = searchParams.get("type"); // "hospital" or "doctor"
   
   const [page, setPage] = useState(true);
-
   useEffect(() => {
     if (type === "hospital") setPage(true);
     if (type === "doctor") setPage(false);
   }, [type]);
-
+  
   return <div>{page ? <SignIn page={page} setPage={setPage}/> : <SignIn2 />}</div>;
+}
+
+export default function RegisterPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RegisterContent />
+    </Suspense>
+  );
 }
