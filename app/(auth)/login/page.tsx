bb64bc06473ee2f8115e5a6ce2e5e20b74bc591b
Raounak Hadil Kaoua <raounak.hadil.kaoua@ensia.edu.dz>
2026-01-02 21:20:30 +0100

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import LogIn from './LogIn';
 import '../../../assets/styles/LogIn.css';
+import { Suspense } from 'react';
+
+function LoginContent() {
+  return (
+    <div className="body">
+      <LogIn />
+    </div>
+  );
+}
 
 export default function LogInPage() {
-	return (
-			<div className="body">
-				<LogIn />
-			</div>
-	);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginContent />
+    </Suspense>
+  );
 }
