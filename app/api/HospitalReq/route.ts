bec47cb3dc3ea50dce4ba5dba44504720c110b9b
Raounak Hadil Kaoua <raounak.hadil.kaoua@ensia.edu.dz>
2026-01-03 22:49:13 +0100

    Update route.ts

--- app/api/HospitalReq/route.ts
+++ app/api/HospitalReq/route.ts
@@ -1,18 +1,29 @@
-/* | column_name      |
-| ---------------- |
-| id               |
-| hospital_from_id |
-| hospital_to_id   |
-| blood_type       |
-| priority         |
-| units_needed     |
-| notes            |
-| status           | */
 import { NextRequest, NextResponse } from 'next/server';
-import { supabase } from '@/lib/supabaseClient';
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
 export async function GET() {
     try {
+        const cookieStore = await cookies();
+        const supabase = createServerClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL!,
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+          {
+            cookies: {
+              getAll() {
+                return cookieStore.getAll();
+              },
+              setAll(cookiesToSet) {
+                try {
+                  cookiesToSet.forEach(({ name, value, options }) =>
+                    cookieStore.set(name, value, options)
+                  );
+                } catch {}
+              },
+            },
+          }
+        );
+
         const { data, error } = await supabase
             .from('hospitals_requests')
             .select(`
@@ -22,6 +33,7 @@ export async function GET() {
             `);
 
         if (error) return NextResponse.json({ error: error.message }, { status: 400 })
+
         return NextResponse.json({ data }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ error: "invalid request" }, { status: 500 });
